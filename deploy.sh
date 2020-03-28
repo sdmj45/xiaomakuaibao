685827c650c83382327a47e5676810c231a7c719
MA Jian <jian.ma-extern@renault.com>
2020-03-28 15:09:05 +0100

    add deploy

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-git add public && git commit -m "site updated" && git subtree push --prefix=public  https://github.com/sdmj45/xiaomakuaibao.git master
+function lazygit() {
+    git add public
+    git commit -m "site updated"
+    git subtree push --prefix=public  https://github.com/sdmj45/xiaomakuaibao.git master
+}
+lazygit
