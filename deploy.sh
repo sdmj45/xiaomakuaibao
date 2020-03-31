9945314943d9d5e4013a0984ac388f6d9e69b8e5
MA Jian <jian.ma-extern@renault.com>
2020-03-31 18:52:33 +0200

    remove push to bitbucket

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,5 @@ function lazygit() {
     git add public
     git commit -m "site updated"
     git subtree push --prefix=public  https://github.com/sdmj45/xiaomakuaibao.git master
-    git push
 }
 lazygit
