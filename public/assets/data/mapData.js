d9d4adfee3b2ccd6954f57dcb11467d20334cd38
MA Jian <jian.ma-extern@renault.com>
2020-03-31 18:17:34 +0200

    site updated

--- public/assets/data/mapData.js
+++ public/assets/data/mapData.js
@@ -1,1 +1,1 @@
-var mapData=[['fr-occ', 1285],['fr-pac', 1927],['fr-idf', 7660],['fr-nor', 789],['fr-ges', 5479],['fr-cvl', 645],['fr-naq', 912],['fr-hdf', 1753],['fr-pdl', 459],['fr-bfc', 1569],['fr-ara', 2093],['fr-bre', 707]]
+var mapData=[['fr-occ', 1082],['fr-pac', 1927],['fr-idf', 7660],['fr-nor', 688],['fr-ges', 5479],['fr-cvl', 1157],['fr-naq', 912],['fr-hdf', 1753],['fr-pdl', 379],['fr-bfc', 1569],['fr-ara', 2093],['fr-bre', 603]]
